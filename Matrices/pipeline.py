a19f7443f1593ffa21e64f2d5248cd7be4a14626
MahatKC <mkarvat@gmail.com>
2021-07-13 20:05:15 -0300

    file created

--- Matrices/pipeline.py
+++ Matrices/pipeline.py
@@ -2,3 +2,15 @@ import numpy as np
 import time
 from prism import create_prism
 
+"""
+Variáveis:
+VRP -> VRP
+P -> P
+Vetor View-up -> Y
+Tipo de projeção (paralela axonométrica/perspectiva) -> Matriz de projeção
+Distancia do plano Near -> n
+Distancia do plano Far -> f
+Distancia ao plano de projeção -> Xp, Yp, Zp
+Limites do mundo -> Xmin, Xmax, Ymin, Ymax
+Limites do plano de projeção -> Umax, Umin, Vmax, Vmin
+"""
