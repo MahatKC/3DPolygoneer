6b7b527b1c62fc191247a3030ad76aed734ecab4
Lucas <Lucas-cs1998@hotmail.com>
2021-07-27 20:26:58 -0300

    Class Screen saving the objects

--- drawing.py
+++ drawing.py
@@ -1,7 +1,8 @@
 from DataStructure import Object
-from Matrices.pipeline import pipeline_full
+import Matrices.pipeline
 import numpy as np
 from tkinter import *
+
 
 class Screen():
     def __init__(self, frame):
@@ -10,9 +11,18 @@ class Screen():
         self.canvas = Canvas(frame, width = int(width*0.7), height = int(height*(0.9)), bg = "white")
 
     def draw(self, object):
+        self.objects[self.number_objects] = []
         for i in range(0, object.numberFaces):
-            x = self.canvas.create_polygon(object.getCoordinates(i), outline='blue', fill='light blue', width=2, tags = "objeto")
-            
+            self.objects[self.number_objects].append(self.canvas.create_polygon(object.getCoordinates(i), outline='blue', fill='light blue', width = 2, tags = "objeto"))
+        self.number_objects = self.number_objects + 1
+
+    def deleteObject(self, face):
+        for i in range(0, self.number_objects):
+            if face in self.objects[i]:
+                return self.objects[i]
+
+                
+
 
 window = Tk()
 window.title('The Marvelous Polygoneer')
@@ -28,8 +38,9 @@ UserInterface.place(x = int(width*0.75), y = int(height * 0.01))
 
 
 #drawing = Canvas(frameDrawingInterface, width = int(width*0.7), height = int(height*(0.9)), bg = "white") 
-drawing = Screen()
-drawing.pack()
+drawing = Screen(frameDrawingInterface)
+
+drawing.canvas.pack()
 
 
 
@@ -39,19 +50,15 @@ poliedro_teste = np.array([[30, 35, 25, 20,   30],
                            [ 1,  1,  1,  1,    1]])
 
 
-obj4 = pipeline_full(poliedro_teste, 50, 15, 30, 20, 6, 15, 0, 1, 0, 10, 40, True, 17, -8, 8, -5, 5, 320, 0, 240, 0)
-
-
-
-def which_object(x, y):
-    x = 10
 
 def object_clicked(event):
     print("voce cricou no objeto")
-    drawing.delete("current")
-    which_object(event.x, event.y)
+    object = drawing.deleteObject(drawing.canvas.find_withtag("current")[0])
+    for i in object:
+        drawing.canvas.delete(i)
 
-drawing.tag_bind("objeto", "<Button-1>", object_clicked)
+
+drawing.canvas.tag_bind("objeto", "<Button-1>", object_clicked)
 
 def draw_objects(event):
     obj = Object(450, 150, 150, 150, 60, 100, 20)
@@ -61,20 +68,20 @@ def draw_objects(event):
     obj5 = Object(650, 150, 250, 250, 60, 100, 3)
     obj6 = Object(1000, 400, 250, 250, 60, 100, 18)
 
-    draw(obj)
-    draw(obj2)
-    draw(obj3)
-    draw(obj4)
-    draw(obj5)
-    draw(obj6)
+    drawing.draw(obj)
+    drawing.draw(obj2)
+    drawing.draw(obj3)
+    drawing.draw(obj4)
+    drawing.draw(obj5)
+    drawing.draw(obj6)
 
 
 def erase(event):
     if event.char == ' ':
         print("test")
-        drawing.delete(ALL)
+        drawing.canvas.delete(ALL)
 
-drawing.bind_all('<space>', erase)
-drawing.bind_all('<x>', draw_objects)
+drawing.canvas.bind_all('<space>', erase)
+drawing.canvas.bind_all('<x>', draw_objects)
 
 window.mainloop()
