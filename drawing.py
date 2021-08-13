b310d62b11fe1c333470ceac1936a0c51ab888a4
robertaalcantara <roberta_alcantara@outlook.com.br>
2021-08-13 18:05:21 -0300

    funções para atalhos do teclado

--- drawing.py
+++ drawing.py
@@ -111,25 +111,63 @@ def move_object_z(value):
 #rotObjectX mandando o value -> rotação
 #rotObjectY mandando o value -> rotação
 #rotObjectZ mandando o value -> rotation
+translationValue = 5
+scaleValue = 5
+rotationValue = 5
 
-translation_value = 5
 def move_x_left():
-    moveObject(-translation_value, 0, 0)
+    moveObject(-translationValue, 0, 0)
 
 def move_x_right():
-    move_object_x(value_move)
-
-"""def move_y_up():
-    move_object_y(-value_move)
-
-def move_y_down():
-    move_object_y(value_move)
+    moveObject(translationValue, 0, 0)
 
 def move_z_front():
-    move_object_z(-value_move)
+    moveObject(0, 0, translationValue)
 
 def move_z_back():
-    move_object_z(value_move)"""
+    moveObject(0, 0, -translationValue)
+
+def move_y_up():
+    moveObject(0, translationValue, 0)
+
+def move_y_down():
+    moveObject(0, -translationValue, 0)
+
+def scale_x_less():
+    scaleObject(-scaleValue, 0, 0)
+
+def scale_x_more():
+    scaleObject(scaleValue, 0, 0)
+
+def scale_z_less():
+    scaleObject(0, 0, -scaleValue)
+
+def scale_z_more():
+    scaleObject(0, 0, scaleValue)
+
+def scale_y_less():
+    scaleObject(0, -scaleValue, 0)
+
+def scale_y_more():
+    scaleObject(0, scaleValue, 0)
+
+def rot_x_left():
+    rotObjectX(-rotationValue)
+
+def rot_x_right():
+    rotObjectX(rotationValue)
+
+def rot_z_front():
+    rotObjectZ(rotationValue)
+
+def rot_z_back():
+    rotObjectZ(-rotationValue)
+
+def rot_y_up():
+    rotObjectY(rotationValue)
+
+def rot_y_down():
+    rotObjectY(-rotationValue)
 
 drawing.canvas.bind_all('<q>', move_x_left)
 drawing.canvas.bind_all('<a>', move_x_right)
@@ -137,7 +175,7 @@ drawing.canvas.bind_all('<w>', move_z_front)
 drawing.canvas.bind_all('<s>', move_z_back)
 drawing.canvas.bind_all('<e>', move_y_up)
 drawing.canvas.bind_all('<d>', move_y_down)
-"""
+
 drawing.canvas.bind_all('<r>', scale_x_less)
 drawing.canvas.bind_all('<f>', scale_x_more)
 drawing.canvas.bind_all('<t>', scale_z_less)
@@ -151,4 +189,3 @@ drawing.canvas.bind_all('<i>', rot_z_front)
 drawing.canvas.bind_all('<k>', rot_z_back)
 drawing.canvas.bind_all('<o>', rot_y_up)
 drawing.canvas.bind_all('<l>', rot_y_down)
-"""
